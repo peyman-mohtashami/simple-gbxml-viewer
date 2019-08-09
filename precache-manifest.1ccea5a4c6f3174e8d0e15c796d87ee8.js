1d5adcbb94b0da1d1fd98526bdb9e7a76eec823a
peyman-mohtashami <p.mohtashami@gmail.com>
2019-08-09 17:13:35 +0200

    Updates

---
+++ precache-manifest.1ccea5a4c6f3174e8d0e15c796d87ee8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f1831b69b6adad06dfde03a988f680",
+    "url": "/simple-gbxml-viewer/index.html"
+  },
+  {
+    "revision": "57fa36bdd4ca51c8ec40",
+    "url": "/simple-gbxml-viewer/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1bfd6f6447a53451b090",
+    "url": "/simple-gbxml-viewer/static/js/2.0e7c9110.chunk.js"
+  },
+  {
+    "revision": "57fa36bdd4ca51c8ec40",
+    "url": "/simple-gbxml-viewer/static/js/main.f6297d14.chunk.js"
+  },
+  {
+    "revision": "5ab583a22789917200d2",
+    "url": "/simple-gbxml-viewer/static/js/runtime~main.731b7702.js"
+  }
+]);
